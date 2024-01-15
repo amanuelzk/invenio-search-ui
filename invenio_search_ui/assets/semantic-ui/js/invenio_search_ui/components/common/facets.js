4aa7e04879712416dc690caaad12a8b5fccfa2b3
emanda hailu <emandahailu65@gmail.com>
2024-01-15 15:11:06 +0300

    removed the comments

--- invenio_search_ui/assets/semantic-ui/js/invenio_search_ui/components/common/facets.js
+++ invenio_search_ui/assets/semantic-ui/js/invenio_search_ui/components/common/facets.js
@@ -1,8 +1,3 @@
-// This file is part of InvenioRDM
-// Copyright (C) 2022 CERN.
-// Invenio Search Ui is free software; you can redistribute it and/or modify it
-// under the terms of the MIT License; see LICENSE file for more details.
-
 import { i18next } from "@translations/invenio_search_ui/i18next";
 import React from "react";
 import {
